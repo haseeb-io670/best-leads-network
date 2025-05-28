c0cc7b3c82826196e567dee6d91e7128c3db4228
Aliyan-12 <alyannasir225@gmail.com>
2025-05-29 03:04:25 +0500

    added num.

--- src/components/sections/MvaInsurance.tsx
+++ src/components/sections/MvaInsurance.tsx
@@ -181,11 +181,11 @@ const MvaInsurance = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
                 </svg>
               </Link>
-              <Link href="/contact-us" className="secondary-button">
+              <Link href="tel:+18882563257" className="secondary-button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <a href="tel:+18882563257"><span>Call Us</span></a>
+                <span>Call Us</span>
               </Link>
             </div>
           </div>
